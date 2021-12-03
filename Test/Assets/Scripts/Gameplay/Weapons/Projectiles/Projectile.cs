e8daa5f372a0abb444a514ca0439da137d45d13c
DaniilMoi <moiseev@179.ru>
2021-12-03 17:52:13 +0300

    initial commit

---
+++ Test/Assets/Scripts/Gameplay/Weapons/Projectiles/Projectile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Gameplay.Helpers;
+using UnityEngine;
+
+namespace Gameplay.Weapons.Projectiles
+{
+    public abstract class Projectile : MonoBehaviour, IDamageDealer
+    {
+
+        [SerializeField]
+        private float _speed;
+
+        [SerializeField] 
+        private float _damage;
+
+
+        private UnitBattleIdentity _battleIdentity;
+
+
+        public UnitBattleIdentity BattleIdentity => _battleIdentity;
+        public float Damage => _damage;
+
+        
+
+        public void Init(UnitBattleIdentity battleIdentity)
+        {
+            _battleIdentity = battleIdentity;
+        }
+        
+
+        private void Update()
+        {
+            Move(_speed);
+        }
+
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log(other.ToString());
+            var damagableObject = other.gameObject.GetComponent<IDamagable>();
+            Debug.Log($"{damagableObject.BattleIdentity} {BattleIdentity}");
+            if (damagableObject != null 
+                && damagableObject.BattleIdentity != BattleIdentity)
+            {
+                Debug.Log("Damage object!");
+                damagableObject.ApplyDamage(this);
+            }
+        }
+        
+
+
+        protected abstract void Move(float speed);
+    }
+}
